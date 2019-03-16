7a64057d3072bf1a29cd760cb64b119376194743
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-03-16 15:01:03 +0100

    Correct everything

--- stepMaker.js
+++ stepMaker.js
@@ -22,7 +22,7 @@ function makeStep(choosenOption){
     }  
   }
   else{
-    if(mapa[endpoint.x/minimumStep][endpoint.y/minimumStep] == 2)
+    if(mapa[endpoint.y/minimumStep][endpoint.x/minimumStep] == 2)
     {
 
     }
@@ -79,7 +79,7 @@ function stepOut(endpoint,value){
   return point;  
 };
 function getPossibleSteps(endpoint, lastStep){
-  if(mapa[endpoint.x/minimumStep][endpoint.y/minimumStep] != 0){
+  if(mapa[endpoint.y/minimumStep][endpoint.x/minimumStep] != 0){
     return [
       {x:endpoint.x+lastStep.x,y:endpoint.y-lastStep.y+ minimumStep},
       { x:endpoint.x + lastStep.x - minimumStep, y:endpoint.y - lastStep.y + minimumStep},
@@ -105,7 +105,7 @@ function getPossibleSteps(endpoint, lastStep){
 
 function step(endpoint, value, lastStep){ 
   var point={ x:0, y:0} ;
-  if(mapa[endpoint.x/minimumStep][endpoint.y/minimumStep] != 0){
+  if(mapa[endpoint.y/minimumStep][endpoint.x/minimumStep] != 0){
     switch(value) { 
       case 1:
         if(!first) {
