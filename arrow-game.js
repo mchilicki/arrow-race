6efff92f2a6d1a4d78ccea54cd54c56f23942ef7
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-03-16 02:06:45 +0100

    Change some library to clean Javascript with canvas drawing

--- arrow-game.js
+++ arrow-game.js
@@ -1,46 +1,6 @@
-var Q;
-
-window.addEventListener("load",function() {
-
-    Q = window.Q =  Quintus().include("Sprites, Scenes, Input").setup(
-        { 
-            width: 1000, 
-            height: 1000,
-            scaleToFit: true
-        });
-
-    Q.Sprite.extend("Arrow",{
-        init:function(p) {
-            this._super(p, {
-                asset: "arrow.png",
-                x: 100, 
-                y: 50,
-                scale: 0.1,
-                angle: 0,
-            }); 
-        }
-    });
-
-    Q.scene("level", function(stage) {
-        var width = 1;
-        for (var height = 0; height < 3; height++){
-            var tanOfAngle = (height + 1) / width;
-            var arrow = new Q.Arrow();
-            arrow.p.angle = toDegrees(Math.atan(tanOfAngle));
-            stage.insert(arrow);  
-            arrows.push(arrow);
-        }            
-    });    
-
-    Q.load("arrow.png",function() {
-        Q.stageScene("level");
-        Q.gameLoop(function(dt) {
-            Q.clear();   
-            for (var i = 0; i < arrows.length; i++){
-                arrows[i].update(dt);
-                arrows[i].render(Q.ctx);
-            }                     
-        });
-    });
+$(document).ready(function(){    
+    var c = document.getElementById("gridCanvas");
+    var ctx = c.getContext("2d");
+    drawGrid(ctx, 800, 600);
+    drawGridArrow(ctx, 20, 20, 35, 35);
 });
-  
