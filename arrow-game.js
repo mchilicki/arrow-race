82ece288e55b6a53a00ed0d1f453df1553a7bafe
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-03-16 08:38:02 +0100

    Refactor arrow-game

--- arrow-game.js
+++ arrow-game.js
@@ -18,32 +18,37 @@ var timesPreviousChoosenOptionWasChoosen = 0;
 
 var minimumTimesPreviousChoosenOptionNeedToBeChoosen = 50;
 
+function getChoosenOption(valueFromLuna)
+{
+    if (isBetweenLeftClosed(valueFromLuna, -1, -0.875))
+            choosenOption = 1;
+    else if (isBetweenLeftClosed(valueFromLuna, -0.875, -0.625))
+        choosenOption = 2;
+    else if (isBetweenLeftClosed(valueFromLuna, -0.625, -0.375))
+        choosenOption = 3;
+    else if(isBetweenLeftClosed(valueFromLuna, -0.375, -0.125))
+        choosenOption = 4;
+    else if (isBetweenLeftClosed(valueFromLuna, -0.125, 0.125))
+        choosenOption = 5;
+    else if (isBetweenLeftClosed(valueFromLuna, 0.125, 0.375))
+        choosenOption = 6;
+    else if (isBetweenLeftClosed(valueFromLuna, 0.375, 0.625))
+        choosenOption = 7;
+    else if (isBetweenLeftClosed(valueFromLuna, 0.625, 0.875))
+        choosenOption = 8;
+    else
+        choosenOption = 1;
+    return choosenOption;
+}
 $(document).ready(function() {    
     var c = document.getElementById("gridCanvas");
     var ctx = c.getContext("2d");
     drawGrid(ctx, 800, 600);
     drawGridArrow(ctx, arrowStartPoint.x, arrowStartPoint.y, arrowEndPoint.x, arrowEndPoint.y);
     controller = new EGZOController();
-    controller.onvalue = function(value)
+    controller.onvalue = function(valueFromLuna)
     {        
-        if (isBetweenLeftClosed(value, -1, -0.875))
-            choosenOption = 1;
-        else if (isBetweenLeftClosed(value, -0.875, -0.625))
-            choosenOption = 2;
-        else if (isBetweenLeftClosed(value, -0.625, -0.375))
-            choosenOption = 3;
-        else if(isBetweenLeftClosed(value, -0.375, -0.125))
-            choosenOption = 4;
-        else if (isBetweenLeftClosed(value, -0.125, 0.125))
-            choosenOption = 5;
-        else if (isBetweenLeftClosed(value, 0.125, 0.375))
-            choosenOption = 6;
-        else if (isBetweenLeftClosed(value, 0.375, 0.625))
-            choosenOption = 7;
-        else if (isBetweenLeftClosed(value, 0.625, 0.875))
-            choosenOption = 8;
-        else
-            choosenOption = 1;
+        choosenOption = getChoosenOption(valueFromLuna);
         if (previousChoosenOption == choosenOption) {
             timesPreviousChoosenOptionWasChoosen++;
         } else {
