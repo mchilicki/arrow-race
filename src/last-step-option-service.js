75d7e1b080c26d1d77d089f038c598ecffe9c8b3
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-03-22 00:05:26 +0100

    Semi correct for last global variables in step-maker

--- src/last-step-option-service.js
+++ src/last-step-option-service.js
@@ -16,23 +16,23 @@ class LastStepOptionService {
     }
 
     getTopLeftPoint(endPoint, lastStep) {
-        return { x: endPoint.x + lastStep.x - this._minimumStep, y: endPoint.y - lastStep.y - this._minimumStep }
+        return { x: endPoint.x + lastStep.x - this._minimumStep, y: endPoint.y - lastStep.y - this._minimumStep };
     }
 
     getTopPoint(endPoint, lastStep) {
-        return { x: endPoint.x + lastStep.x, y: endPoint.y - lastStep.y - this._minimumStep }
+        return { x: endPoint.x + lastStep.x, y: endPoint.y - lastStep.y - this._minimumStep };
     }
 
     getTopRightPoint(endPoint, lastStep) {
-        return { x: endPoint.x + lastStep.x + this._minimumStep, y: endPoint.y - lastStep.y - this._minimumStep }
+        return { x: endPoint.x + lastStep.x + this._minimumStep, y: endPoint.y - lastStep.y - this._minimumStep };
     }
 
     getRightPoint(endPoint, lastStep) {
-        return { x: endPoint.x + lastStep.x + this._minimumStep, y: endPoint.y - lastStep.y }
+        return { x: endPoint.x + lastStep.x + this._minimumStep, y: endPoint.y - lastStep.y };
     }
 
     getBottomRightPoint(endPoint, lastStep) {
-        return { x: endPoint.x + lastStep.x + this._minimumStep, y: endPoint.y - lastStep.y + this._minimumStep }
+        return { x: endPoint.x + lastStep.x + this._minimumStep, y: endPoint.y - lastStep.y + this._minimumStep };
     }
 
     getAllPossiblePoints(endPoint, lastStep) {
